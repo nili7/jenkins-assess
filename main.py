5cabc87e3aeb0dc8cda200c44f9c54e213d8a300
nilee <36537205+nili7@users.noreply.github.com>
2020-08-18 14:48:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+""" Simple banking functions """
+
+# Creates new account whenever file is run
+BALANCE = 0
+print('Your Account is Created.')
+print('Your Balance = %d' %BALANCE)
+
+def deposit(bal, amt):
+    """ To add money to account """
+    bal += amt
+    print('Your New Balance = %d' %bal)
+    return bal
+
+def withdraw(bal, amt):
+    """ To withdraw money from account """
+    # only if sufficient balance
+    if amt > bal:
+        print('Insufficient Balance!')
+    else:
+        bal -= amt
+        print('Your Remaining Balance = %d' %bal)
+    return bal
+
+def enquiry(bal):
+    """ Prints account details """
+    print('Your Balance = %d' %bal)
+
+print("1. Deposit\n2. Withdraw\n3. Enquiry\n4. Exit\nEnter your choice: ")
+choice = int(input())
+
+while choice != 4:
+    if choice == 1:
+        amount = int(input('Enter the amount to deposit:'))
+        BALANCE = deposit(BALANCE, amount)
+    elif choice == 2:
+        amount = int(input('Enter the amount to withdraw:'))
+        BALANCE = withdraw(BALANCE, amount)
+    elif choice == 3:
+        enquiry(BALANCE)
+    elif choice == 4:
+        break
+    print("\n1. Deposit\n2. Withdraw\n3. Enquiry\n4. Exit\nEnter your choice: ")
+    choice = int(input())
---
+++ test.py
@@ -0,0 +1,30 @@
+""" Program to test Banking """
+import io
+import unittest
+import unittest.mock
+from banking import deposit
+from banking import withdraw
+from banking import enquiry
+
+class TestBanking(unittest.TestCase):
+    """ Class to test Banking """
+    def test_deposit(self):
+        """ Test that it can deposit """
+        result = deposit(0, 100)
+        self.assertEqual(result, 100)
+    def test_withdraw_sufficient(self):
+        """ Test that it can withdraw when sufficient balance """
+        result = withdraw(100, 10)
+        self.assertEqual(result, 90)
+    def test_withdraw_insufficient(self):
+        """ Test that it cannot withdraw when insufficient balance """
+        result = withdraw(100, 1000)
+        self.assertEqual(result, 100)
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_enquire(self, mock_stdout):
+        """ Test that it can enquire """
+        enquiry(100)
+        self.assertEqual(mock_stdout.getvalue(), 'Your Balance = 100\n')
+
+if __name__ == '__main__':
+    unittest.main()
